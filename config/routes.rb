1ef4bbe8ad1da4cb617ef75be6f51049053a3450
Fernando Almeida <nando.a.g.martines@gmail.com>
2023-05-30 12:30:07 -0300

    create cars controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :cars
+
+  resources :cars do
+    collection do
+      get 'brand/:brand', action: 'brand', as: 'brand'
+      get 'model/:model', action: 'model', as: 'model'
+      get 'price/:price', action: 'price', as: 'price'
+    end
+  end
+
   resources :orders
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
