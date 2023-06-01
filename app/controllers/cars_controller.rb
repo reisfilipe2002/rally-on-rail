0234e46bd62b124bca6d3b1f7f78b5ab092da71c
filipe reis <reisfilipe102@gmail.com>
2023-06-01 18:54:08 -0300

    Correct

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,12 +3,9 @@ class CarsController < ApplicationController
   def index
     @cars = policy_scope(Car)
     if params[:query].present?
-      sql_subquery = <<~SQL
-        cars.brand @@ :query
-        OR cars.model @@ :query
-        OR cars.price @@ :query
-      SQL
-      @cars = @cars.joins(:brand).where(sql_subquery, query: params[:query])
+      @cars = Car.search_by_brand_and_model(params[:query]).where(sold: false)
+    else
+      @cars = Car.where(sold: false)
     end
   end
 
