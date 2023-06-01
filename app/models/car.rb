93223afc3a1bf884350fd42266b5789dea9c7eba
filipe reis <reisfilipe102@gmail.com>
2023-06-01 16:52:22 -0300

    my_cars

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,11 @@ class Car < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
   has_one :order
+
+  # include PgSearch::Model
+  # pg_search_scope :search_by_model_and_brand,
+  # against: [ :model, :brand ],
+  # using: {
+  #   tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  # }
 end
